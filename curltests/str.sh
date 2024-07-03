8cc02fa5af6f349deaa5b168d7c574cad8186cdd
Jeesus Bock <jeesus.bock@gmail.com>
2024-07-03 13:49:46 +0300

    Reorganized. Added second TCL script.

---
+++ curltests/str.sh
@@ -0,0 +1,4 @@
+curl -H 'Content-Type: application/json' \
+      -d '{ "str":"pienellä" }' \
+      -X POST \
+      http://localhost:8080/tcl/str
---
+++ scripts/str.tcl
@@ -0,0 +1,2 @@
+# Simple string manipulation, turning input to uppercase for now
+string toupper [dict get $data str]
